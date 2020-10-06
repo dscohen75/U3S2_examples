ffd430f0b913fc0ae954703fbb0cf3c1c60dc623
Nicholas dela Fuente <51771867+nwdelafu@users.noreply.github.com>
2020-10-06 13:32:14 -0600

    Final version of Module 2 examples from lesson

--- my_examples/ETL_pipeline_sl2pg.py
+++ my_examples/ETL_pipeline_sl2pg.py
@@ -16,6 +16,7 @@ def connect_to_pg(pg_dbname, pg_user, pg_password, pg_host, extraction_db='rpg_d
 def execute_query(curs, query):
   return curs.execute(query)  #executes query depending on the cursor we have passed in
 
+
 # Queries
 # creating characters list
 get_characters = "SELECT * FROM charactercreator_character"
@@ -40,7 +41,6 @@ CREATE TABLE charactercreator_character (
 # (name, level, exp, hp, strength, intelligence, dexterity, wisdom)
 # VALUES""" + str(characters[0][1:]) + ";"
 
-
 # check to see if we actually filled table
 check_table_exist = "SELECT * FROM charactercreator_character"
 
@@ -53,11 +53,11 @@ if __name__ == "__main__":
   characters = execute_query(sl_curs, get_characters)
   execute_query(pg_curs, create_pg_character_table) # creating table in pg db
   for character in characters:
-      insert_character = """
-    INSERT INTO charactercreator_character
-    (name, level, exp, hp, strength, intelligence, dexterity, wisdom)
-    VALUES""" + str(character[1:]) + ";"
-      execute_query(pg_curs, insert_character)
+    insert_character = """
+      INSERT INTO charactercreator_character
+      (name, level, exp, hp, strength, intelligence, dexterity, wisdom)
+      VALUES""" + str(character[1:]) + ";"
+    execute_query(pg_curs, insert_character)
 
   pg_conn.commit() # only need to commit pg since no changes on SQLite
   execute_query(pg_curs, check_table_exist) # checks if it exist in PG DB
