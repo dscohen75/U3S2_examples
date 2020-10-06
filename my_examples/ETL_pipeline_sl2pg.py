14a218f6007d5ef9ce46cea341e105e0b615e367
Nicholas dela Fuente <51771867+nwdelafu@users.noreply.github.com>
2020-10-06 13:30:58 -0600

    Finals version of Module 2 examples from lesson

--- my_examples/ETL_pipeline_sl2pg.py
+++ my_examples/ETL_pipeline_sl2pg.py
@@ -8,13 +8,13 @@ pg_host = "topsy.db.elephantsql.com"
 
 
 def connect_to_pg(pg_dbname, pg_user, pg_password, pg_host, extraction_db='rpg_db.sqlite3'):
-    """ Connects to DB - return sl_conn & pg_conn """
-    sl_conn = sqlite3.connect(extraction_db) # local SQLite db
-    pg_conn = psycopg2.connect(dbname=pg_dbname, user=pg_user, password=pg_password, host=pg_host) # postgreSQL db
-    return sl_conn, pg_conn
+  """ Connects to DB - return sl_conn & pg_conn """
+  sl_conn = sqlite3.connect(extraction_db) # local SQLite db
+  pg_conn = psycopg2.connect(dbname=pg_dbname, user=pg_user, password=pg_password, host=pg_host) # postgreSQL db
+  return sl_conn, pg_conn
 
 def execute_query(curs, query):
-    return curs.execute(query).fetchall() #executes query depending on the cursor we have passed in
+  return curs.execute(query)  #executes query depending on the cursor we have passed in
 
 # Queries
 # creating characters list
@@ -46,18 +46,18 @@ check_table_exist = "SELECT * FROM charactercreator_character"
 
 
 if __name__ == "__main__":
-    sl_conn, pg_conn = connect_to_pg(pg_dbname, pg_user, pg_password, pg_host)
-    sl_curs = sl_conn.cursor()
-    pg_curs = pg_conn.cursor()
-    print(pg_curs)
-    characters = execute_query(sl_curs, get_characters)
-    execute_query(pg_curs, create_pg_character_table) # creating table in pg db
-    for character in characters:
-        insert_character = """
-      INSERT INTO charactercreator_character
-      (name, level, exp, hp, strength, intelligence, dexterity, wisdom)
-      VALUES""" + str(character[1:]) + ";"
-        execute_query(pg_curs, insert_character)
+  sl_conn, pg_conn = connect_to_pg(pg_dbname, pg_user, pg_password, pg_host)
+  sl_curs = sl_conn.cursor()
+  pg_curs = pg_conn.cursor()
+  print(sl_curs)
+  characters = execute_query(sl_curs, get_characters)
+  execute_query(pg_curs, create_pg_character_table) # creating table in pg db
+  for character in characters:
+      insert_character = """
+    INSERT INTO charactercreator_character
+    (name, level, exp, hp, strength, intelligence, dexterity, wisdom)
+    VALUES""" + str(character[1:]) + ";"
+      execute_query(pg_curs, insert_character)
 
-    pg_conn.commit() # only need to commit pg since no changes on SQLite
-    execute_query(pg_curs, check_table_exist) # checks if it exist in PG DB
+  pg_conn.commit() # only need to commit pg since no changes on SQLite
+  execute_query(pg_curs, check_table_exist) # checks if it exist in PG DB
